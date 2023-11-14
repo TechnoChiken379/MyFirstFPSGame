8c2671e76f0a86bc1a8ab5ff97b0bcac9e1a447c
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-14 15:30:32 +0100

    update

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    private float currentSpeed;
+    public float walkingSpeed = 10f;
+    public float runningSpeed = 15;
 
+    private float moveX;
+    private float moveZ;
 
     public CharacterController characterController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSpeed = walkingSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveX = Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime;
+        moveZ = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
     }
 }
