640d0486e02a3aa0d55ca834617b9bc02ef93ba1
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-14 14:51:13 +0100

    Update playerMovement.cs

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+
+
+    public CharacterController characterController;
+
     // Start is called before the first frame update
     void Start()
     {
